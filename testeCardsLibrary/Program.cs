02fee52c335df07b2ce8aa30ac08553c84c1024f
pds2012 <webseller.douglas.almeida@gmail.com>
2012-05-17 23:03:42 -0300

    Baralho

---
+++ testeCardsLibrary/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Copyright (c) 2012 Adriano Quilião de Oliveira e Douglas Almeida.
+ * Este programa é licenciado pelos termos da Licença Pública Geral Menor GNU
+ * em sua versão 3 e esta disponivel www.gnu.org/copyleft/lesser.txt
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace testeCardsLibrary
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CardsLibrary.Deck baralho = new CardsLibrary.Deck();
+            //baralho.shuffle(200);
+            //baralho.cut(26);
+            //baralho.getFistCard();
+            //baralho.getLastCard();
+            //baralho.moveFirstForTheLastPosition();
+            //baralho.moveLastForTheFirstPosition();
+            //baralho.discard(baralho.getFistCard());
+            Console.WriteLine("-------------<BARALHO>---------------");
+            foreach (CardsLibrary.Card card in baralho)
+                Console.WriteLine("Naipe -> " + card.naipe + "  Carta -> " + card.index.ToString());
+            Console.ReadLine();
+        }
+
+    }
+}
